c9322bfcee7b8c1e43171bb6b95a77b6f9632d8f
Giga Khizanishvili <khizanag@gmail.com>
2026-03-30 18:22:35 +0400

    Fix navigation: close button placement and duplicate destination
    - Move NavigatorView close button from .topBarLeading to .topBarTrailing
    - Remove duplicate .navigationDestination(for: Country.self) in ContentView
      (already registered centrally in CoordinatedNavigationStack)

--- Geografy/App/Navigation/NavigatorView.swift
+++ Geografy/App/Navigation/NavigatorView.swift
@@ -82,7 +82,7 @@ private extension NavigatorView {
 
     @ToolbarContentBuilder
     var dismissToolbarItem: some ToolbarContent {
-        ToolbarItem(placement: .topBarLeading) {
+        ToolbarItem(placement: .topBarTrailing) {
             CircleCloseButton { coordinator.dismiss() }
         }
     }
