5e563cb032aa2c14b7eb7a65decab7a4274a66e1
Giga Khizanishvili <khizanag@gmail.com>
2026-03-31 15:24:49 +0400

    Extract HideCloseButton environment key to separate file

--- Geografy/Navigation/CoordinatedNavigationStack.swift
+++ Geografy/Navigation/CoordinatedNavigationStack.swift
@@ -1,23 +1,5 @@
 import SwiftUI
 
-// MARK: - Hide Close Button Environment Key
-private struct HideCloseButtonKey: EnvironmentKey {
-    static let defaultValue = false
-}
-
-extension EnvironmentValues {
-    var hideCloseButton: Bool {
-        get { self[HideCloseButtonKey.self] }
-        set { self[HideCloseButtonKey.self] = newValue }
-    }
-}
-
-extension View {
-    func hideNavigationCloseButton(_ hidden: Bool = true) -> some View {
-        environment(\.hideCloseButton, hidden)
-    }
-}
-
 // MARK: - Coordinated Navigation Stack
 struct CoordinatedNavigationStack<Root: View>: View {
     @Environment(\.dismiss) private var dismiss
